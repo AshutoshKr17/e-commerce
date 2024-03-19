7725fbf8c4592b7d13b3ae6458599aef83d87034
Amartya-Bhardwaj <amartya1142001@gmail.com>
2024-03-19 23:40:40 +0530

    Adding routes for USER

--- backend/main.go
+++ backend/main.go
@@ -7,12 +7,18 @@ import (
 	"github.com/AshutoshKr17/e-commerce/backend/db"
 	"github.com/AshutoshKr17/e-commerce/backend/routes"
 	"github.com/AshutoshKr17/e-commerce/backend/views"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	ctx := context.Background()
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	r.Use(cors.New(config)) 
 	gin.SetMode(gin.ReleaseMode)
 	db.ConnectDatabase()
 	err := db.DB.Ping(ctx,  nil)
